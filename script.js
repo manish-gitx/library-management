d61bf20c8804e6489ab2bde3e505e5708faff98b
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-12-03 04:12:36 +0530

    updated local

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const addButton = document.querySelector('.sub-input');
 const main = document.querySelector('.info');
 let count = 1;
 
+// Load data from local storage
+window.addEventListener('DOMContentLoaded', () => {
+    const data = JSON.parse(localStorage.getItem('libraryData'));
+    if (data) {
+        data.forEach(item => {
+            createRow(item);
+        });
+    }
+});
+
 addButton.addEventListener('click', (event) => {
     event.preventDefault();
     var nameValue = nameInput.value;
@@ -50,6 +60,7 @@ addButton.addEventListener('click', (event) => {
     del.addEventListener('click', () => {
         newDiv.remove();
         updateLi1Values();
+        updateLocalStorage();
     });
 
     function getdate() {
@@ -61,11 +72,10 @@ addButton.addEventListener('click', (event) => {
         return ans;
     }
 
-
     count++;
     updateTable();
+    updateLocalStorage();
 });
-
 
 function updateLi1Values() {
     const divs = document.querySelectorAll('.row');
@@ -76,13 +86,71 @@ function updateLi1Values() {
 
     const lastDiv = divs[divs.length - 1];
     const lastLi1 = lastDiv.querySelector('li:first-child');
-    var check=parseInt(lastLi1.innerText);
-    if(check==1){
-        count=1;
+    var check = parseInt(lastLi1.innerText);
+    if (check == 1) {
+        count = 1;
+    } else {
+        count = parseInt(lastLi1.innerText) + 1;
     }
-    else{
-        count = parseInt(lastLi1.innerText)+1;
-    }
-    
+}
 
+function updateLocalStorage() {
+    const rows = document.querySelectorAll('.row');
+    const data = [];
+    rows.forEach(row => {
+        const li1 = row.querySelector('li:first-child');
+        const li2 = row.querySelector('li:nth-child(2)');
+        const li3 = row.querySelector('li:nth-child(3)');
+        const li4 = row.querySelector('li:nth-child(4)');
+        const li5 = row.querySelector('li:nth-child(5)');
+        const item = {
+            count: li1.innerText,
+            book: li2.innerText,
+            name: li3.innerText,
+            date: li4.innerText,
+            id: li5.innerText
+        };
+        data.push(item);
+    });
+    localStorage.setItem('libraryData', JSON.stringify(data));
+}
+
+function createRow(item) {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('row');
+    main.appendChild(newDiv);
+
+    const li1 = document.createElement('li');
+    const li2 = document.createElement('li');
+    const li3 = document.createElement('li');
+    const li4 = document.createElement('li');
+    const li5 = document.createElement('li');
+    const del = document.createElement('button');
+
+    li1.innerText = item.count;
+    newDiv.appendChild(li1);
+
+    li2.innerText = item.book;
+    newDiv.appendChild(li2);
+
+    li3.innerText = item.name;
+    newDiv.appendChild(li3);
+
+    li4.innerText = item.date;
+    newDiv.appendChild(li4);
+
+    li5.innerText = item.id;
+    newDiv.appendChild(li5);
+
+    del.innerText = "Delete";
+    del.classList.add('del-btn');
+    newDiv.appendChild(del);
+
+    newDiv.scrollIntoView({ behavior: 'smooth' });
+
+    del.addEventListener('click', () => {
+        newDiv.remove();
+        updateLi1Values();
+        updateLocalStorage();
+    });
 }
